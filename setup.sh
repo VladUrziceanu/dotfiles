7ca25c52f02d61f9059c69309ff7d3a37a35947e
Vlad Urziceanu <vlad.urziceanu@gmail.com>
2024-10-02 07:23:53 +0300

    add setup script and initial config files

---
+++ setup.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+execute() {
+  cmd=$1
+  msg=$2
+
+  eval "${cmd}"
+  if [[ $? ]]; then
+    # Print output in green.
+    printf "\e[0;32m  [✔] %s\e[0m\n" "${msg}"
+  else
+    # Print output in red.
+    printf "\e[0;31m  [✖] %s\e[0m\n" "${msg}"
+  fi
+}
+
+install_omz() {
+  ZSH=${HOME}/.oh-my-zsh
+  ZSH_CUSTOM="${ZSH_CUSTOM:-${ZSH}/custom}"
+
+  # Clone or update Oh My Zsh.
+  if [[ ! -d "${ZSH}" ]]; then
+    git clone --quiet --filter=blob:none https://github.com/robbyrussell/oh-my-zsh "${ZSH}"
+  else
+    git -C "${ZSH}" pull --quiet
+  fi
+
+  # Install or update custom oh-my-zsh plugins.
+  CUSTOM_PLUGIN_REPOS=(
+    "https://github.com/Aloxaf/fzf-tab"
+    "https://github.com/zdharma-continuum/fast-syntax-highlighting"
+    "https://github.com/zsh-users/zsh-autosuggestions"
+  )
+  for REPO_URL in "${CUSTOM_PLUGIN_REPOS[@]}"; do
+    PLUGIN_PATH="${ZSH_CUSTOM}/plugins/${REPO_URL##*/}"
+    if [[ ! -d "${PLUGIN_PATH}" ]]; then
+      git clone --quiet --filter=blob:none "${REPO_URL}" "${PLUGIN_PATH}"
+    else
+      git -C "${PLUGIN_PATH}" pull --quiet
+    fi
+  done
+}
+
+install_tmux_themepack() {
+  TMUX_THEMEPACK=${HOME}/.tmux-themepack
+
+  if [[ ! -d "${ZSH}" ]]; then
+    git clone --quiet --filter=blob:none https://github.com/jimeh/tmux-themepack.git "${TMUX_THEMEPACK}"
+  else
+    git -C "${TMUX_THEMEPACK}" pull --quiet
+  fi
+}
+
+link_file() {
+  local source=$1
+  local target=$2
+
+  # We've already symlinked, do nothing.
+  if [[ "$(readlink "${target}")" == "${source}" ]]; then
+    return
+  fi
+
+  # If the target location exists and it's not our target symlink, we create a backup.
+  if [[ -e "${target}" ]]; then
+    epoch=$(date +%s)
+    execute "mv ${target} ${target}.${epoch}.bak" "Backing up ${target} → ${target}.${epoch}.bak"
+  fi
+
+  # Symlink the dotfile.
+  execute "ln -fs ${source} ${target}" "Linking ${target} → ${source}"
+}
+
+main() {
+  install_omz
+  install_tmux_themepack
+
+  FILES_TO_SYMLINK=($(find configs -mindepth 1 -maxdepth 1 -type f))
+  for dotfile in "${FILES_TO_SYMLINK[@]}"; do
+    sourceFile="$(pwd)/${dotfile}"
+    targetFile="${HOME}/.$(basename "${dotfile}")"
+
+    link_file "$sourceFile" "$targetFile"
+  done
+}
+
+main
